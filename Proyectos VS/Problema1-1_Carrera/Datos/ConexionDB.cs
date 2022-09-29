62ff916934d7e2dcc0fbf911bfa5d7e32f2e835d
NicoArroyo1 <84149390+NicoArroyo1@users.noreply.github.com>
2022-09-29 09:11:40 -0300

    comit

--- Proyectos VS/Problema1-1_Carrera/Datos/ConexionDB.cs
+++ Proyectos VS/Problema1-1_Carrera/Datos/ConexionDB.cs
@@ -16,7 +16,7 @@ namespace Problema1_1_Carrera.Datos
         public ConexionDB()
         {
             cnn = new SqlConnection();
-            cnn.ConnectionString = Properties.Resources.CadenaConexion1;
+            cnn.ConnectionString = Properties.Resources.CadenaConexion;
         }
 
         public DataTable SPConsulta(string nombreSP)
