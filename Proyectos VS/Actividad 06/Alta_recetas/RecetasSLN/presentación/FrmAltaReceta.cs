bb11915043abf2b2d914b543a879577d9eccff58
NicoArroyo1 <84149390+NicoArroyo1@users.noreply.github.com>
2022-09-29 17:47:32 -0300

    modelo parcial

---
+++ Proyectos VS/Actividad 06/Alta_recetas/RecetasSLN/presentación/FrmAltaReceta.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecetasSLN
+{
+    public partial class FrmAltaReceta : Form
+    {
+        
+        public FrmAltaReceta()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(txtNombre.Text))
+            {
+                MessageBox.Show("Debe ingresar un nombre válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                txtNombre.Focus();
+                return;
+            }
+            else if (cboTipo.SelectedIndex == -1)
+            {
+                MessageBox.Show("Debe seleccionar un tipo de receta válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            else if (dgvDetalles.Rows.Count == 0)
+            {
+                MessageBox.Show("Debe insertar al menos un detalle...", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+        
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("¿Está seguro que desea cancelar?", "Salir", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                this.Dispose();
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+    }
+}
